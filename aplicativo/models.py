d776f19717a4d66d5f9d1ef8f31df33b72b035d6
Wagner <wagner.confessor@escolar.ifrn.edu.br>
2024-11-07 17:47:13 -0300

    Criação do modelo Car

--- aplicativo/models.py
+++ aplicativo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Car(models.Model):
+    model = models.CharField(max_length=50, null=False)
+    brand = models.CharField(max_length=50, null=False)
+    year = models.IntegerField(null=False)
+    filename = models.CharField(max_length=50, null=False)
 # Create your models here.
